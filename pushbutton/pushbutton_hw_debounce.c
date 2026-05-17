3aa5530dff37dbb09227096018d5150237f362fe
Ryuichi Lin <ryuno107lin@gmail.com>
2026-05-16 22:44:10 -0400

    feat: add hardware debounce pushbutton variant for mouse rev b

---
+++ pushbutton/pushbutton_hw_debounce.c
@@ -0,0 +1,73 @@
+/*-------------------------------- FILE INFO ---------------------------------*/
+/* Filename           : pushbutton.c                                          */
+/*                                                                            */
+/* Implementation for a pushbutton device                                     */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*                               Include Files                                */
+/*----------------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "eic_hal.h"
+#include "eic_hal_config.h"
+#include "gpio_hal.h"
+#include "gpio_hal_config.h"
+#include "pushbutton.h"
+
+/*----------------------------------------------------------------------------*/
+/*                         Private Function Prototypes                        */
+/*----------------------------------------------------------------------------*/
+/* none */
+
+/*----------------------------------------------------------------------------*/
+/*                               Private Globals                              */
+/*----------------------------------------------------------------------------*/
+static const struct eic_hal_handler *eic_handler = NULL;
+static const struct eic_handle *config_pushbutton_eic = NULL;
+static const struct gpio_hal_handler *gpio_handler = NULL;
+static const struct gpio_handle *config_pushbutton_gpio = NULL;
+
+static volatile uint32_t pressed_count = 0u;
+
+/*----------------------------------------------------------------------------*/
+/*                         Public Function Definitions                        */
+/*----------------------------------------------------------------------------*/
+void init_pushbutton(void)
+{
+    eic_handler = get_eic_hal_handler();
+    config_pushbutton_eic = get_config_pushbutton_eic_handle();
+    gpio_handler = get_gpio_hal_handler();
+    config_pushbutton_gpio = get_config_pushbutton_gpio_handle();
+
+    eic_handler->set_external_callback(config_pushbutton_eic, pushbutton_isr);
+}
+
+void deinit_pushbutton(void)
+{
+    pressed_count = 0u;
+}
+
+uint32_t get_pushbutton_count(void)
+{
+    return pressed_count;
+}
+
+void clear_pushbutton_count(void)
+{
+    pressed_count = 0u;
+}
+
+/*----------------------------------------------------------------------------*/
+/*                        Private Function Definitions                        */
+/*----------------------------------------------------------------------------*/
+void pushbutton_isr(void)
+{
+    while (gpio_handler->read_gpio_pin(config_pushbutton_gpio) == false) {
+        /* given default HIGH from pull-up, wait for release back to HIGH */
+    }
+
+    pressed_count++;
+}
---
+++ pushbutton/tests/test_pushbutton_hw_debounce.cpp
@@ -0,0 +1,217 @@
+/*================================ FILE INFO =================================*/
+/* Filename           : test_pushbutton.cpp                                   */
+/*                                                                            */
+/* Test implementation for pushbutton.c                                       */
+/*                                                                            */
+/*============================================================================*/
+
+/*============================================================================*/
+/*                               Include Files                                */
+/*============================================================================*/
+extern "C"
+{
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "eic_hal.h"
+#include "eic_hal_config.h"
+#include "gpio_hal.h"
+#include "gpio_hal_config.h"
+#include "pushbutton.h"
+
+}
+
+#include <CppUTest/TestHarness.h>
+#include <CppUTestExt/MockSupport.h>
+
+/*============================================================================*/
+/*                            Mock Implementations                            */
+/*============================================================================*/
+const struct eic_hal_handler *get_eic_hal_handler(void)
+{
+    return static_cast<const struct eic_hal_handler *>(
+        mock().actualCall("get_eic_hal_handler")
+        .returnConstPointerValue()
+    );
+}
+
+const struct eic_handle *get_config_pushbutton_eic_handle(void)
+{
+    return static_cast<const struct eic_handle *>(
+        mock().actualCall("get_config_pushbutton_eic_handle")
+        .returnConstPointerValue()
+    );
+}
+
+const struct gpio_hal_handler *get_gpio_hal_handler(void)
+{
+    return static_cast<const struct gpio_hal_handler *>(
+        mock().actualCall("get_gpio_hal_handler")
+        .returnConstPointerValue()
+    );
+}
+
+const struct gpio_handle *get_config_pushbutton_gpio_handle(void)
+{
+    return static_cast<const struct gpio_handle *>(
+        mock().actualCall("get_config_pushbutton_gpio_handle")
+        .returnConstPointerValue()
+    );
+}
+
+/*============================================================================*/
+/*                             Public Definitions                             */
+/*============================================================================*/
+/* eic mocks */
+/* the definition of "struct eic_handle" is hardware specific, so we mock */
+struct eic_handle {
+    uint32_t mock_value;
+};
+
+const struct eic_handle mock_config_pushbutton_eic_handle = {0};
+
+bool external_callback_set = false;
+
+void dummy_init_eic(void) {}
+void dummy_deinit_eic(void) {}
+void mock_set_external_callback(const struct eic_handle *handle, 
+                                     void (*callback)(void))
+{
+    CHECK(callback != nullptr);
+    if (handle == &mock_config_pushbutton_eic_handle) {
+        external_callback_set = true;
+    }
+}
+
+const struct eic_hal_handler mock_eic_handler = {
+    dummy_init_eic,
+    dummy_deinit_eic,
+    mock_set_external_callback
+};
+
+/* -------------------------------------------------------------------------- */
+/* gpio mocks */
+/* the definition of "struct gpio_handle" is hardware specific, so we mock */
+struct gpio_handle {
+    uint32_t mock_value;
+};
+
+const struct gpio_handle mock_config_pushbutton_gpio_handle = {0};
+
+bool mock_pushbutton_input = false;
+
+void dummy_init_gpio(void) {}
+void dummy_deinit_gpio(void) {}
+bool mock_read_gpio_pin(const struct gpio_handle *handle)
+{
+    bool pin_reading = false;
+    if (handle == &mock_config_pushbutton_gpio_handle) {
+        pin_reading = mock_pushbutton_input;
+    }
+    return pin_reading;
+}
+void dummy_write_gpio_pin(const struct gpio_handle *handle, bool value){}
+void dummy_toggle_gpio_pin(const struct gpio_handle *handle) {}
+
+const struct gpio_hal_handler mock_gpio_handler = {
+    dummy_init_gpio,
+    dummy_deinit_gpio,
+    mock_read_gpio_pin,
+    dummy_write_gpio_pin,
+    dummy_toggle_gpio_pin
+};
+
+void set_pushbutton_released(void)
+{
+    mock_pushbutton_input = true; /* HIGH for release */
+}
+
+/* -------------------------------------------------------------------------- */
+/* test helpers */
+void reset_test_flags(void)
+{
+    external_callback_set = false;
+}
+
+void reset_mock_input(void)
+{
+    mock_pushbutton_input = false;
+}
+
+void init_config_pushbutton_with_cpputest_checks(void)
+{
+    mock().expectOneCall("get_eic_hal_handler")
+        .andReturnValue(&mock_eic_handler);
+    mock().expectOneCall("get_config_pushbutton_eic_handle")
+        .andReturnValue(&mock_config_pushbutton_eic_handle);
+    mock().expectOneCall("get_gpio_hal_handler")
+        .andReturnValue(&mock_gpio_handler);
+    mock().expectOneCall("get_config_pushbutton_gpio_handle")
+        .andReturnValue(&mock_config_pushbutton_gpio_handle);
+    init_pushbutton();
+}
+
+void pushbutton_isr_with_cpputest_checks(void)
+{
+    set_pushbutton_released();
+    pushbutton_isr();
+    CHECK(get_pushbutton_count() == 1);
+}
+
+/*============================================================================*/
+/*                                 Test Group                                 */
+/*============================================================================*/
+TEST_GROUP(PushbuttonTests)
+{
+    void setup() override
+    {
+        reset_test_flags();
+        reset_mock_input();
+        mock().clear();
+    }
+
+    void teardown() override
+    {
+        mock().checkExpectations();
+        mock().clear();
+        reset_mock_input();
+        reset_test_flags();
+    }
+};
+
+/*============================================================================*/
+/*                                    Tests                                   */
+/*============================================================================*/
+TEST(PushbuttonTests, InitCallsFunctions)
+{
+    init_config_pushbutton_with_cpputest_checks();
+}
+
+TEST(PushbuttonTests, InitSetsExternalCallback)
+{
+    init_config_pushbutton_with_cpputest_checks();
+    CHECK(external_callback_set);
+}
+
+TEST(PushbuttonTests, IsrIncrementsCountWithDebounce)
+{
+    init_config_pushbutton_with_cpputest_checks();
+    pushbutton_isr_with_cpputest_checks();
+}
+
+TEST(PushbuttonTests, DeinitResetsCount)
+{
+    init_config_pushbutton_with_cpputest_checks();
+    pushbutton_isr_with_cpputest_checks();
+    deinit_pushbutton();
+    CHECK(get_pushbutton_count() == 0);
+}
+
+TEST(PushbuttonTests, ClearPushbuttonCountResetsCount)
+{
+    init_config_pushbutton_with_cpputest_checks();
+    pushbutton_isr_with_cpputest_checks();
+    clear_pushbutton_count();
+    CHECK(get_pushbutton_count() == 0);
+}
